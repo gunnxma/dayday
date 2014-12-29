7601a95877ba2f7fd08d7f7bc15ce2b9921914d6
gunnxma <gunnxma@gmail.com>
2014-12-29 15:54:00 +0800

    index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,45 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
+
+
+$(function(){
+	$(".pop").each(function() {
+	  var $pElem = $(this);
+	  $pElem.popover({
+	    html: true,
+	    trigger: "manual",
+	    title: getPopoverTitle($pElem.attr("id")),
+	    content: getPopoverContent($pElem.attr("id")),
+	    container: 'body',
+	    animation: false,
+	    template: '<div class="popover" role="tooltip"><div class="arrow"></div><div class="popover-content"></div></div>'
+	  });
+	}).on("mouseenter",
+		function() {
+		  var _this = this;
+		  $(this).popover("show");
+		  $(".popover").on("mouseleave",
+			  function() {
+			    $(_this).popover('hide');
+			  }
+			);
+	}).on("mouseleave",
+				function() {
+				  var _this = this;
+				  setTimeout(function() {
+				    if (!$(".popover:hover").length) {
+				      $(_this).popover("hide");
+				    }
+				  },
+				  100);
+				});
+
+		function getPopoverTitle(target) {
+		  return $("#" + target + "_content > h2.popover-title").html();
+		};
+
+		function getPopoverContent(target) {
+		  return $("#" + target + "_content > div.popover-content").html();
+		};
+});
