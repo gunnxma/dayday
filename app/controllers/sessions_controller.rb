2e82c9be5d6371cd52dc280b9b74bb6d64031159
gunnxma <gunnxma@gmail.com>
2015-01-11 10:17:58 +0800

    login validation finished

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,15 +7,15 @@ class SessionsController < ApplicationController
 	  user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
 	  #cookies[:user_id] = user.id
 	  cookies.permanent[:user_id] = user.id
-	  redirect_to root_url, :notice => "Signed in!"
+	  redirect_to root_url, :notice => "登录成功"
 	end
 
 	def destroy
 	  cookies.delete :user_id
-	  redirect_to root_url, :notice => "Signed out!"
+	  redirect_to root_url, :notice => "已退出"
 	end
 
 	def failure
-		redirect_to root_url, alert: "Authentication failed, please try again"
+		redirect_to new_session_url, notice: "用户名或密码错误，请重新登录！"
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   post "/auth/:provider/callback", to: "sessions#create"
   get "/signout", to: "sessions#destroy", :as => :signout
+  get "/auth/failure", to: "sessions#failure"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
