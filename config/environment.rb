452cf0e65d9820be630ec0fb037129fe21483380
gunnxma <gunnxma@gmail.com>
2015-01-22 13:27:53 +0800

    fixed html dom error

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ENV['QQ_CONNECT_APP_KEY'] = 'AAAAA'
-ENV['QQ_CONNECT_APP_SECRET'] = 'BBBBB'
