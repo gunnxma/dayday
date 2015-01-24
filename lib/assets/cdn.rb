1960319540185cc392603ec237fe636089906214
gunnxma <gunnxma@gmail.com>
2015-01-24 20:32:57 +0800

    sync assets to upyun

---
+++ lib/assets/cdn.rb
@@ -0,0 +1,8 @@
+require 'ftp_sync'
+namespace :assets do
+  desc 'sync assets to cdns'
+  task :cdn => :environment do 
+    ftp = FtpSync.new("v0.ftp.upyun.com", "xinqidou/xinqidou-assets", "macnmq1983",true)
+    ftp.sync("#{Rails.root}/public/assets/", "/assets")
+  end
+end
