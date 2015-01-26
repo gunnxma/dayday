2c363fdddae83bdb32acccc899c8a52458d941e9
gunnxma <gunnxma@gmail.com>
2015-01-26 11:42:46 +0800

    change auth call back route

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   resources :sessions
 
-  post "/auth/:provider/callback", to: "sessions#create"
+  get "/auth/:provider/callback", to: "sessions#create"
   get "/signout", to: "sessions#destroy", :as => :signout
   get "/auth/failure", to: "sessions#failure"
 
