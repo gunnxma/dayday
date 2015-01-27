01bd0cd3e48f9e798fc2a905cbaf169519d85500
gunnxma <gunnxma@gmail.com>
2015-01-27 21:57:46 +0800

    change index per(5) to per(15)

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,13 +1,5 @@
 class IndexController < ApplicationController
 	def index
-		@things = Thing.order(id: :desc).page(params[:page]).per(5)
+		@things = Thing.order(id: :desc).page(params[:page]).per(15)
 	end
-
-	def ajax_page
-    @things = Thing.order(id: :desc).page(params[:page]).per(5)
-  end
-
-  def endless_page
-  	@things = Thing.order(id: :desc).page(params[:page]).per(5)
-  end
 end
